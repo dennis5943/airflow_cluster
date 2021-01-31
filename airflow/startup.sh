c1d7870a388dd9413d9a5b837e6344b5e913cba1
dennis5943 <dennis5943@gmail.com>
2021-01-31 17:09:26 +0000

    2.0.0.9

--- airflow/startup.sh
+++ airflow/startup.sh
@@ -1,21 +1,4 @@
-service postgresql start
-su - postgres -c "createdb airflow"
-su - postgres bash -c "psql -c \"CREATE USER airflow WITH PASSWORD '12345678';\""
-
-cp -f airflow.cfg /root/airflow/airflow.cfg
-
-airflow db reset -y
-
-##python default_user.py
-
-# create an admin user
-airflow users create \
-    --username $AIRFLOW_USER \
-    -p $AIRFLOW_PASSWORD \
-    --firstname airflow \
-    --lastname airflow \
-    --role Admin \
-    --email $AIRFLOW_EMAIL
+bash startup_postgresql.sh
 
 airflow webserver -p 8080 & airflow scheduler
 airflow worker
